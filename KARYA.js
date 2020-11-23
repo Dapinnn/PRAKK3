e96f108dad917502b27e4707640aaa6ed439e4c5
Dapinnn <69647338+Dapinnn@users.noreply.github.com>
2020-11-23 18:06:28 +0700

    Add files via upload

---
+++ KARYA.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class karya extends React.Component{
+    render(){
+        return (
+            <div class=" p-2 mb-1 bg-light text-black">
+                <div class="container-fluid" >
+                <div class="alert alert-info" role="alert">
+                <h1 class="display-4" className="text-center">MESSANGE</h1>
+                <div class="jumbotron">
+                    
+                    <h2 class="display-4">NADIN AMIZAH</h2>
+                    <br />
+                    <p class="lead">Kita beranjak dewasa. Jauh terburu seharusnya. </p>
+                    <p class="lead">Bagai bintang yang jatuh.</p>
+                    <p class="lead">Jauh terburu waktu. Mati lebih cepat.</p>
+                </div>
+                </div>
+                </div>
+            </div>
+        )
+    }
+}
+export default karya;
---
+++ TENTANG SAYA.js
@@ -0,0 +1,52 @@
+import React from 'react';
+class tentangsaya extends React.Component{
+    render(){
+        return (
+            <div class=" p-2 mb-1 bg-light text-black">
+                <div class="container-fluid" >
+                <div class="alert alert-dark" role="alert">
+                <h3 class="display-4">Tentang DAPINN</h3>
+                <p class="lead">Tentang Apa yang mungkin anda tidak tau</p>
+                </div>
+            <div class="card">
+                <div class="card-header">
+                <h3 class="card-title">Hobi</h3>
+                </div>
+            <div class="card-body">
+            <h5 class="text-secondary">Saya sengat senang berenang, makan, dan jalan jalan.</h5>
+            </div>
+            </div>
+            <div class="card">
+                <div class="card-header">
+                <h3 class="card-title">Hope</h3>
+                </div>
+            <div class="card-body">
+            <h5 class="text-secondary">Keinginan saya adalah memiliki keluarga yang bahagia, </h5>
+            <h5 class="text-secondary">dan yang utama membanggakan kedua orang tua</h5>
+            </div>
+            </div>
+            <div class="card">
+                <div class="card-header">
+                <h3 class="card-title">Pendidikan</h3>
+            </div>
+            <div class="card-body" >
+                <p class="text-secondary">TK : TK DHARMA WANITA</p>
+                <p class="text-secondary">SD : SDN TUREN 04</p>
+                <p class="text-secondary">SMP: MTSN 02 MALANG</p>
+                <p class="text-secondary">SMK: SMK TELKOM MALANG</p>
+            </div>
+            </div>
+            <div class="card">
+                <div class="card-header">
+                <h3 class="card-title">Alamat</h3>
+            </div>
+            <div class="card-body">
+                <h5 class="text-secondary"> gmail : davina_shafa_28rpl@student.smktelkom-mlg.sch.id</h5>
+            </div>
+            </div>
+            </div>
+            </div> 
+        )
+    }
+}
+export default tentangsaya;
